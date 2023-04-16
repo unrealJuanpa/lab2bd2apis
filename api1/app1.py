b88ece31907a4f26148fe1def30fbabf07c4e545
unrealJuanpa <juanpacloud@gmail.com>
2023-04-15 21:08:25 -0600

    finalizado

--- api1/app1.py
+++ api1/app1.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, jsonify
 from dbstructure import *
 import datetime
+import requests
+import json
 
 
 app = Flask(__name__)
+uriapi2 = 'http://127.0.0.1:6000'
 db.connect()
 
 @app.route('/tipocontribuyente', methods=['GET', 'POST'])
@@ -44,7 +47,22 @@ def manage_datosventa(id=None):
             ff = datetime.datetime.now()
             Datosventa.create(**req_data, fechahora=ff)
             Logs.create(informacion=f"Se ha creado el registro de venta para el cliente {req_data['nombrecliente']} en el momento: {ff}")
-            return jsonify({'message': 'Data added successfully'})
+
+
+            idv = list(Datosventa.select().dicts())[-1]['id']
+            ntc = str(list(Tipocontribuyente.select().where(Tipocontribuyente.id == req_data['tipocontribuyente']).dicts())[0]['nombre'])
+
+            headers = {'Content-type': 'application/json'}
+            dt = {
+                "idventa": idv,
+                "nitcliente": req_data['nitcliente'],
+                "nombrecliente": req_data['nombrecliente'],
+                "fechahora": str(ff),
+                "total": req_data['total'],
+                "nombretipocontribuyente": ntc
+            }
+            response = requests.post(url=uriapi2+'/contabilidad', data=json.dumps(dt), headers=headers) # algo esta pasando en esta linea
+            return jsonify({'message': response.json()['message']})
         elif request.method == 'PUT':
             Datosventa.update(**req_data).where(Datosventa.id == id).execute()
             Logs.create(informacion=f"Se ha modificado el registro de datos venta con id = {id} en el momento: {datetime.datetime.now()}")
